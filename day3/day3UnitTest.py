ba65071447bcd64f06398087383fce11e2edbcf9
jiji7879 <mrdavidlai1@gmail.com>
2024-12-03 01:43:30 -0600

    Day 3

--- day3/day3UnitTest.py
+++ day3/day3UnitTest.py
@@ -1,7 +1,6 @@
 import unittest
 import day3
 
-
 class MyTestCase(unittest.TestCase):
     def test_part_1(self):
         sampleText = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
